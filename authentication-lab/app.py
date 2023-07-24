e87de5e69ded76fa55c71646c9590866e98b6c5d
shirazto24-meet <139688798+shirazto24-meet@users.noreply.github.com>
2023-07-24 16:25:50 +0300

    Update app.py

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -4,7 +4,6 @@ import pyrebase
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
-
 config = {
   "apiKey": "AIzaSyDRa5Ivx-Az9YPibYY0HsQGXWqlwZcZRhs",
   "authDomain": "waterbottle-1ac10.firebaseapp.com",
@@ -12,7 +11,7 @@ config = {
   "storageBucket": "waterbottle-1ac10.appspot.com",
   "messagingSenderId": "917953316977",
   "appId": "1:917953316977:web:15de65c998dd452059f3d0",
-  "measurementId": "G-H255FGG9SS"
+  "measurementId": "G-H255FGG9SS",
   "databaseURL":""
 }
 
@@ -22,21 +21,35 @@ auth = firebase.auth()
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
-    return render_template("signin.html")
+    error= ""
+    if request.method=='POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user'] = auth.sign_in_with_email_and_password(email,password)
+            return redirect(url_for('add_tweet'))
+        except:
+            error = 'signin failed'
+            return render_template("signin.html")
+    else:
+        return render_template('signin.html')
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    if method == 'POST':
-        error= ""
+    error= ""
+    if request.method == 'POST':
+        email= request.form['email']
+        password = request.form['password']
         try:
-            email= request.form['email']
-            password = request.form['password']
             login_session['user'] = auth.create_user_with_email_and_password(email, password)
-            return redirect(url_for('add_html'))
+            return redirect(url_for('add_tweet.html'))
         except:
             error = "auth failed"
-            return render_template("signup.html")
+        return render_template("signup.html")
+    else:
+        return render_template('signup.html')
+
 
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
