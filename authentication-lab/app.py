1b254c3c9243caf3f8bdc56887a09968c5609f2d
shirazto24-meet <139688798+shirazto24-meet@users.noreply.github.com>
2023-07-25 18:17:33 +0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -12,11 +12,12 @@ config = {
   "messagingSenderId": "917953316977",
   "appId": "1:917953316977:web:15de65c998dd452059f3d0",
   "measurementId": "G-H255FGG9SS",
-  "databaseURL":""
+  "databaseURL":"https://waterbottle-1ac10-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
+db = firebase.database()
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -41,20 +42,50 @@ def signup():
     if request.method == 'POST':
         email= request.form['email']
         password = request.form['password']
+        full_name = request.form['full_name']
+        username = request.form['username']
+        bio = request.form['bio']
+
+        
+
         try:
             login_session['user'] = auth.create_user_with_email_and_password(email, password)
-            return redirect(url_for('add_tweet.html'))
+
+            UID = login_session['user']['localId']
+            user = {"email":email,"password":password,"full_name":full_name,"username": username,"bio":bio }
+            db.child("Users").child(UID).set(user)
+            return redirect(url_for('signin'))
         except:
             error = "auth failed"
-        return render_template("signup.html")
+            return render_template("signup.html")
     else:
         return render_template('signup.html')
 
 
-
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
-    return render_template("add_tweet.html")
+    error = ""
+    try:
+        UID = login_session['user']['localId']
+    except:
+        return redirect(url_for("signin"))
+
+    if request.method == 'POST':
+        title = request.form['title']
+        text = request.form['text']
+        
+        tweet = {'title':title, 'text':text,'uid':UID}
+        db.child('Tweets').push(tweet)
+        return redirect(url_for("all_tweets"))
+
+
+    else:
+        return render_template("add_tweet.html")
+
+@app.route('/all_tweets')
+def all_tweets():
+    tweets = db.child("Tweets").get().val()
+    return render_template("tweets.html", tweets=tweets)
 
 
 if __name__ == '__main__':
